7ed10e9baa5972dedfdbafcbf7703fdb4855378e
matthew seo <rongjuni@hotmail.com>
2022-03-24 23:40:54 -0400

    API practice

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
-// console.log("Fetch API");
+console.log("Fetch API");
 
-// let city = document.getElementById("city");
-// let temp = document.getElementById("temp");
-// let form = document.querySelector("#formID");
-// let query = document.getElementById("query");
+let city = document.getElementById("city");
+let temp = document.getElementById("temp");
+let form = document.querySelector("#formID");
+let query = document.getElementById("query");
 
-// form.addEventListener("submit", function (event) {
-//   event.preventDefault();
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-//   let queryCity = query.value;
+  let queryCity = query.value;
 
-//   // in a url, anytihg after ? is a query parameter
-//   // in the format of name = value
-//   // q
-//   // Query parameters are separated by &
-//   // appid
+  // in a url, anytihg after ? is a query parameter
+  // in the format of name = value
+  // q
+  // Query parameters are separated by &
+  // appid
 
-//   let WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${queryCity}&appid=59cc63d45ae4752acbc1749c59637a5a&units=imperial`;
+  let WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=${queryCity}&appid=59cc63d45ae4752acbc1749c59637a5a&units=imperial`;
 
-//   // Fetch returns a promise
-//   // cors - 다른 origin 에서 접속 못하게
-//   fetch(WEATHER_URL, { mode: "cors" })
-//     .then((response) => {
-//       // console.log(response);
+  // Fetch returns a promise
+  // cors - 다른 origin 에서 접속 못하게
+  fetch(WEATHER_URL, { mode: "cors" })
+    .then( (response)=>{
+      // console.log(response);
+      // .json Parses the response body to JSON, JavaScript Object Notation
+      return response.json();
+    })
+    .then( (data)=>{
+      console.log(data);
+      city.textContent = data.name;
+      temp.textContent = data.main.temp + " F";
+    })
+    .catch( (err)=>{
+      console.error(err);
+      city.textContent = err.message;
+    });
+});
 
-//       // .json Parses the response body to JSON, JavaScript Object Notation
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       city.textContent = data.name;
-//       temp.textContent = data.main.temp + " F";
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       city.textContent = err.message;
-//     });
-// });
+
+
+
+
 
 let giphySearch = document.getElementById('giphySearch');
 let giphySubmit = document.getElementById('giphy');
@@ -56,7 +60,7 @@ giphySubmit.addEventListener('submit',function(event){
     })
     .then( (response) => {
         // console.log(response);
-        giphySecond.src = response.data.images.original.url;
+        return giphySecond.src = response.data.images.original.url;
     })
 
     .catch( (err) => {
